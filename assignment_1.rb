b73ae3b46d60b5fce9f856c7a6611b18f492120c
Jordan Hillman <jordan@test.com>
2017-11-07 01:46:26 -0600

    for u logandi

---
+++ assignment_1.rb
@@ -0,0 +1,14 @@
+puts "Welcome to the start of Jor Dawg's coding education."
+
+puts "Type in a number, Logandi."
+
+input = " "
+
+input = gets.to_i
+
+$i = input
+
+puts "Here is your summation, Logandi"
+
+puts ($i**2+$i)/2
+
