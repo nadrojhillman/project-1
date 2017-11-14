7ca1d40c52d85b34762ef94c92caa3fccea30316
Jordan Hillman <jordan@test.com>
2017-11-13 23:39:42 -0600

    part 6 broh

---
+++ assignment_6_client.rb
@@ -0,0 +1,11 @@
+require './assignment_6_server'
+
+# CLIENT FUNCTIONALITY (for Jor-Dawg to implement):
+
+puts "Welcome to part 5 of Jor Dawg's coding education."
+
+
+i = ARGV[0].to_i
+puts summation(i)
+
+
---
+++ assignment_6_server.rb
@@ -0,0 +1,10 @@
+# SERVER FUNCTIONALITY
+def summation(i)
+
+    if (i>0)
+        return (i**2+i)/2
+    else 
+        return (i**2-i)/-2 
+    end
+
+end
